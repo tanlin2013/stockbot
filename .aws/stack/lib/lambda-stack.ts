99820f63679be6778f69b48b40999e1d215c025e
taolin <tanlin2013@gmail.com>
2021-05-16 21:24:23 +0800

    Feature: implement lambda functions & add api gateway

--- .aws/stack/lib/lambda-stack.ts
+++ .aws/stack/lib/lambda-stack.ts
@@ -1,67 +1,69 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
-import * as ecr from '@aws-cdk/aws-ecr';
+import { PythonFunction } from "@aws-cdk/aws-lambda-python";
 import * as apigateway from '@aws-cdk/aws-apigateway';
 import * as events from '@aws-cdk/aws-events';
 import * as targets from '@aws-cdk/aws-events-targets';
-import * as path from 'path';
-
+import * as acm from "@aws-cdk/aws-certificatemanager";
 
 export class LambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    const repo = new ecr.Repository(this, 'Repository');
-
-    new lambda.DockerImageFunction(this, 'ECRFunction', {
-      code: lambda.DockerImageCode.fromEcr(repo),
-    });
-
-    new lambda.Function(this, 'dynamo', {
-      code: lambda.Code.fromAsset(path.join(__dirname, 'dynamo-handler')),
-      handler: 'functions/dynamo/handler.handler',
+    // 👇 Create lambda function - telegram handler
+    const telegramHandler = new PythonFunction(this, 'telegram', {
+      entry: 'src/telegram',
       runtime: lambda.Runtime.PYTHON_3_7,
+      environment: {
+        region: cdk.Stack.of(this).region,
+        TELEGRAM_API_ID: process.env.TELEGRAM_API_ID,
+        TELEGRAM_API_HASH: process.env.TELEGRAM_API_HASH,
+        TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+        TELEGRAM_DB_KEY: process.env.TELEGRAM_DB_KEY,
+        TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
+      },
     });
 
-    new lambda.Function(this, 'telegram', {
-      code: lambda.Code.fromAsset(path.join(__dirname, 'telegram-handler')),
-      handler: 'functions/telegram/handler.handler',
-      runtime: lambda.Runtime.PYTHON_3_7,
-    });
-
-    const lambdaTrading = new lambda.Function(this, 'trading', {
-      code: lambda.Code.fromAsset(path.join(__dirname, 'trading-handler')),
-      handler: 'functions/trading/handler.handler',
-      runtime: lambda.Runtime.PYTHON_3_7,
-    });
-
-    const rule = new events.Rule(this, 'Rule', {
-        schedule: events.Schedule.expression('cron(0 18 * ? MON-FRI *)')
-    });
-    rule.addTarget(new targets.LambdaFunction(lambdaTrading));
-
-    const api = new apigateway.RestApi(this, 'api', {
-      description: 'example api gateway',
+    // API Gateway
+    const api = new apigateway.RestApi(this, 'Telegram-Endpoint', {
+      handler: telegramHandler,
       deployOptions: {
         stageName: 'dev',
       },
-      // 👇 enable CORS
-      defaultCorsPreflightOptions: {
-        allowHeaders: [
-          'Content-Type',
-          'X-Amz-Date',
-          'Authorization',
-          'X-Api-Key',
-        ],
-        allowMethods: ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-        allowCredentials: true,
-        allowOrigins: ['http://localhost:3000'],
-      },
+      // enable CORS
+    //   defaultCorsPreflightOptions: {
+    //     allowHeaders: [
+    //       'Content-Type',
+    //       'X-Amz-Date',
+    //       'Authorization',
+    //       'X-Api-Key',
+    //     ],
+    //     allowMethods: ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    //     allowCredentials: true,
+    //     allowOrigins: ['https://api.telegram.org'],
+    //   },
     });
 
-    // 👇 create an Output for the API URL
+    // 👇 Create lambda function - trading handler
+    new PythonFunction(this, 'trading', {
+      entry: 'src/trading',
+      runtime: lambda.Runtime.PYTHON_3_7,
+    });
+
+    // 👇 Create lambda function - daily examiner
+    // const dailyExaminer = new lambda.Function(this, 'daily_examiner', {
+    //   code: lambda.Code.fromAsset('src/daily_examiner'),
+    //   handler: 'app.handler',
+    //   runtime: lambda.Runtime.PYTHON_3_7,
+    // });
+
+    // Schedule lambda with contab
+    // const rule = new events.Rule(this, 'Rule', {
+    //     schedule: events.Schedule.expression('cron(0 18 * ? MON-FRI *)')
+    // });
+    // rule.addTarget(new targets.LambdaFunction(dailyExaminer));
+
+    // 👇 Create outputs
     new cdk.CfnOutput(this, 'apiUrl', {value: api.url});
 
   }
