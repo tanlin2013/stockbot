f0b81ae977d35e158c967f22cda6732f804f3b0a
taolin <tanlin2013@gmail.com>
2021-05-19 21:56:53 +0800

    Refactor: rename with pascal case

--- .aws/stack/lib/index.ts
+++ .aws/stack/lib/index.ts
@@ -3,6 +3,6 @@ import { LambdaStack } from "./lambda-stack";
 import { DynamoStack } from "./dynamo-stack";
 
 export default function main(app: cdk.Construct) {
-    new LambdaStack(app, "lambda-handlers");
-    new DynamoStack(app, "dynamodb-tables");
+    new LambdaStack(app, "LambdaHandlers");
+    // new DynamoStack(app, "DynamodbTables");
 }
