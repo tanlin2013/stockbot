f494edd5a0c2c49d3dafeb5d6a9ee73fdb918ae2
taolin <tanlin2013@gmail.com>
2021-05-19 21:46:12 +0800

    Fix: env var issue & export api gateway url

--- .aws/stack/lib/lambda-stack.ts
+++ .aws/stack/lib/lambda-stack.ts
@@ -1,31 +1,36 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
-import { PythonFunction } from "@aws-cdk/aws-lambda-python";
 import * as apigateway from '@aws-cdk/aws-apigateway';
-import * as events from '@aws-cdk/aws-events';
-import * as targets from '@aws-cdk/aws-events-targets';
-import * as acm from "@aws-cdk/aws-certificatemanager";
+import { PythonFunction } from '@aws-cdk/aws-lambda-python';
+import axios from 'axios';
 
 export class LambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // 👇 Define layers for custom module dependencies
+    // new lambda.LayerVersion(this, 'DynamoLayer', {
+    //   compatibleRuntimes: [
+    //     lambda.Runtime.PYTHON_3_7,
+    //   ],
+    //   code: lambda.Code.fromAsset('lib/dynamo'),
+    // });
+
     // 👇 Create lambda function - telegram handler
-    const telegramHandler = new PythonFunction(this, 'telegram', {
+    const telegramHandler = new PythonFunction(this, 'TelegramHandler', {
       entry: 'src/telegram',
+      index: 'app.py',
       runtime: lambda.Runtime.PYTHON_3_7,
       environment: {
-        region: cdk.Stack.of(this).region,
-        TELEGRAM_API_ID: process.env.TELEGRAM_API_ID,
-        TELEGRAM_API_HASH: process.env.TELEGRAM_API_HASH,
-        TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-        TELEGRAM_DB_KEY: process.env.TELEGRAM_DB_KEY,
-        TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
+        'TELEGRAM_API_ID': process.env.TELEGRAM_API_ID!,
+        'TELEGRAM_API_HASH': process.env.TELEGRAM_API_HASH!,
+        'TELEGRAM_BOT_TOKEN': process.env.TELEGRAM_BOT_TOKEN!,
+        'TELEGRAM_DB_KEY': process.env.TELEGRAM_DB_KEY!,
       },
     });
 
     // API Gateway
-    const api = new apigateway.RestApi(this, 'Telegram-Endpoint', {
+    const api = new apigateway.LambdaRestApi(this, 'TelegramEndpoint', {
       handler: telegramHandler,
       deployOptions: {
         stageName: 'dev',
@@ -45,13 +50,14 @@ export class LambdaStack extends cdk.Stack {
     });
 
     // 👇 Create lambda function - trading handler
-    new PythonFunction(this, 'trading', {
-      entry: 'src/trading',
-      runtime: lambda.Runtime.PYTHON_3_7,
-    });
+    // const tradingHandler = new lambda.Function(this, 'TradingHandler', {
+    //   code: lambda.Code.fromAsset('src/trading'),
+    //   handler: 'app.handler',
+    //   runtime: lambda.Runtime.PYTHON_3_7,
+    // });
 
     // 👇 Create lambda function - daily examiner
-    // const dailyExaminer = new lambda.Function(this, 'daily_examiner', {
+    // const dailyExaminer = new lambda.Function(this, 'DailyExaminer', {
     //   code: lambda.Code.fromAsset('src/daily_examiner'),
     //   handler: 'app.handler',
     //   runtime: lambda.Runtime.PYTHON_3_7,
@@ -64,7 +70,25 @@ export class LambdaStack extends cdk.Stack {
     // rule.addTarget(new targets.LambdaFunction(dailyExaminer));
 
     // 👇 Create outputs
-    new cdk.CfnOutput(this, 'apiUrl', {value: api.url});
+    new cdk.CfnOutput(this, 'API url', {value: api.url});
+    new cdk.CfnOutput(this, 'Lambda Function Name', {
+      value: telegramHandler.functionName
+    })
+    // new cdk.CfnOutput(this, 'Trading Function Name', {
+    //   value: tradingHandler.functionName
+    // })
 
+    // 👇 Register webhook
+    const url: string = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/setWebhook?url=${api.url}`;
+    
+    const getRequest = async () => {
+      try {
+        const response = await axios.get(url);
+        console.log(response.data);
+      } catch (exception) {
+        process.stderr.write(`ERROR received from ${url}: ${exception}\n`);
+      }
+    };
+    getRequest()
   }
 }
